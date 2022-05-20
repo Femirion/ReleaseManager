163f3636cf40dfdfa11874ba321a65c118e02038
Aleksandr Vostryakov <femirion@gmail.com>
2022-05-20 12:20:42 +0300

    change to record

--- src/main/java/com/femirion/releasemagager/domain/Resource.java
+++ src/main/java/com/femirion/releasemagager/domain/Resource.java
@@ -1,9 +1,4 @@
 package com.femirion.releasemagager.domain;
 
-import lombok.Data;
-
-@Data
-public class Resource {
-    private String name;
-    private int version;
+public record Resource (String name, int version) {
 }
